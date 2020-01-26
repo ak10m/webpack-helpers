a808f350f5e3245bbe1be05a9f807dc9a17f50f1
ak10m <akio.morimoto@airits.jp>
2020-01-26 21:22:21 +0900

    bundle gem

---
+++ lib/webpack/helpers.rb
@@ -0,0 +1,8 @@
+require "webpack/helpers/version"
+
+module Webpack
+  module Helpers
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/webpack/helpers/version.rb
@@ -0,0 +1,5 @@
+module Webpack
+  module Helpers
+    VERSION = "0.1.0"
+  end
+end
